d6737e6c13251c6dd5d7cd81799d5066ed949292
angtroake <32530289+angtroake@users.noreply.github.com>
2018-02-17 11:15:53 -0330

    Early Shape Detection
    Started working on shape detection, this creates another image and checks for corners.  There is still a window for color isolation, but now there's another window for corner count.

--- Python/Test.py
+++ Python/Test.py
@@ -41,6 +41,8 @@ while True:
     ret_val, img = cam.read()       #this gets the frame of the camera at this time
     img = cv2.flip(img, 1)          #flips image horizontally so it makes sense
 
+    shapeImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     outputImage = 0                 #init of the outputImage
     allmask = 0                     #init of variable to contain all masks
 
@@ -51,10 +53,37 @@ while True:
 
         mask = cv2.inRange(img, lower, upper)    #creating a mask of only pixels that have color in range
         allmask += mask                          #adds this mask to all maskes to be rendered
-        
-    outputImage = cv2.bitwise_and(img, img, mask = allmask)   
-    cv2.imshow('cam', np.hstack([img, outputImage]))     #displays image with masks added
+    
+    outputImage = cv2.bitwise_and(img, img, mask = allmask)
 
+    edge_detect = cv2.blur(outputImage, (5,5))      #creting a new image for detecting corners, this is a blured verion of the output image
+    edge_detect = cv2.Canny(edge_detect, 75, 200)   #this turns the image into lines only
+
+    
+    try:   #safety net lol
+        corners = cv2.goodFeaturesToTrack(edge_detect, 100,0.7,50)  #finding corners and putting them into an array
+        if len(corners) > 0:
+            corners = np.int0(corners)   #converting values from long to int i think
+
+            for cor in corners:         #loop through all corners and get x,y and draw circle on edge_detect
+                x,y = cor.ravel()
+                cv2.circle(edge_detect, (x,y), 3, 255, -1)
+
+            #text display
+            cv2.putText(edge_detect, str(len(corners)), (5, 40), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+
+            #checking for shape
+            if len(corners) == 3:
+                cv2.putText(edge_detect, "TRIANGLE", (5, 90), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+            elif len(corners) == 4:
+                cv2.putText(edge_detect, "RECTANGLE", (5, 90), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+
+    except (Exception):
+        None    #safety net
+        
+    cv2.imshow('cam', outputImage)      #displays images
+    cv2.imshow('shape', edge_detect)
+    
     if cv2.waitKey(1) == 27:          #if ESC is pressed, quit program
         break;
         
