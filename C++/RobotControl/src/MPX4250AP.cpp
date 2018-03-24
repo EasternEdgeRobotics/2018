c1dc78208573a7efdd26efc78a7551a2030769bb
Charity <cetalbot@mun.ca>
2018-03-24 11:17:53 -0300

    sync

--- C++/RobotControl/src/MPX4250AP.cpp
+++ C++/RobotControl/src/MPX4250AP.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "MPX4250AP.h"
+#include "maestro.h"
 
 MPX4250AP::MPX4250AP() {
 	// TODO Auto-generated constructor stub
@@ -16,3 +17,50 @@ MPX4250AP::~MPX4250AP() {
 	// TODO Auto-generated destructor stub
 }
 
+#include <observable/property.hpp>
+using namespace observable;
+
+#ifndef MPX4250AP_H_
+#define MPX4250AP_H_
+/**
+ * MPX4250AP Pressure Sensor
+ */
+class MPX4250AP
+{
+public:
+	MPX4250AP();
+	virtual ~MPX4250AP();
+
+#endif /* MPX4250AP_H_ */
+/**
+ * PressureScalar - Scalar value for calculating pressure from voltage.
+ * PressureOffset - Offset value for calculating pressure from voltage.
+ */
+private:
+	double pressureScalar = 0.264909785f;
+	double pressureOffset = 54.20054200f;
+
+public: 
+	observable::Transformer (double, double)
+	calibration = source -> source.map(x = 0.83060f*x + 10.0445f);
+
+private:
+	ADC channel; 
+/*
+ * constructs a new MPX4250AP sensor on the given channel
+ */
+public:
+	MPX4250AP;
+	this.channel = channel;
+
+/* read
+ * 
+ * returns the pressure
+ * 
+ * @output --- returns the pressure
+ */
+public double read();
+{
+return channel.voltage()*pressureOffset-pressureScalar;	
+}
+}
--- C++/RobotControl/src/MPX4250AP.h
+++ C++/RobotControl/src/MPX4250AP.h
@@ -2,29 +2,8 @@
  * MPX4250AP.h
  *
  *  Created on: Mar 3, 2018
- *      Author: Charity
+ *   Author: Charity
  */
-//using namespace observable;
 
-#ifndef MPX4250AP_H_
-#define MPX4250AP_H_
 
-class MPX4250AP
-{
-public:
-	MPX4250AP();
-	virtual ~MPX4250AP();
 
-#endif /* MPX4250AP_H_ */
-/**
- * Scalar value for calculating pressure from voltage.
- */
-double PressureScalar = 0.264909785f;
-/**
- * Offset value for calculating pressure from voltage.
- */
-double PressureOffset = 54.20054200f;
-
-//observable::Transformer (double, double)
-
-};
