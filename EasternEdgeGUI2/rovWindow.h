d17ea60f53c20c8f24c053beb91d83062124a2e3
jrdawe <35874625+jrdawe@users.noreply.github.com>
2018-05-13 15:00:30 -0230

    minor rearrangements + addition of depth reading

--- EasternEdgeGUI2/rovWindow.h
+++ EasternEdgeGUI2/rovWindow.h
@@ -4,18 +4,18 @@
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Slider.H>
 #include <FL/Fl_Value_Slider.H>
-#include <FL/Fl_Round_Button.H>
 #include <FL/Fl_Radio_Light_Button.H>
 #include <FL/Fl_Value_Input.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Toggle_Button.H>
+#include <FL/Fl_Value_Output.H>
 #include <array>
 
 using namespace std;
 
 /*
 * The GUI for Eastern Edge! User interface for controlling the ROV.
-* Has 7 sliders, 1 for total power and the rest for the 6 axis of motion
+* Has 9 sliders, 1 for total power and the rest for the 6 axis of motion + fore and aft
 * 10 buttons, each for a different preset for motion
 * This is Ver. 2! Hopefully new and improved...
 */
@@ -29,6 +29,7 @@ public:
 	void updateSliderArray(double sliderValues[]);
 	void getGo(double angleValues[]);
 	void toggleBool(bool& a, bool& e);
+	////////////////////////////////
 	Fl_Value_Slider* mainPower;
 	Fl_Value_Slider* surge;
 	Fl_Value_Slider* heave;
@@ -36,6 +37,8 @@ public:
 	Fl_Value_Slider* yaw;
 	Fl_Value_Slider* pitch;
 	Fl_Value_Slider* roll;
+	Fl_Value_Slider* fore;
+	Fl_Value_Slider* aft;
 	////////////////////////////////
 	Fl_Radio_Light_Button* button;
 	Fl_Radio_Light_Button* button2;
@@ -58,4 +61,8 @@ public:
 	////////////////////////////////
 	Fl_Toggle_Button* acoustic;
 	Fl_Toggle_Button* electromagnet;
+	////////////////////////////////
+	Fl_Value_Output* depth;
+
+	Fl_Button* killSwitch;
 };
